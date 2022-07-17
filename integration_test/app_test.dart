73a0bcfe05b665fee8e6020390fb71667ea76e61
AlbertBaby <albertbaby266192@gmail.com>
2022-07-17 12:47:24 +0530

    Fixed tap on favourite button

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,11 +2,9 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:music_app/domin/usecases/artists/search_for_artist.dart';
 import 'package:music_app/main.dart' as app;
 import 'package:integration_test/integration_test.dart';
 import 'package:music_app/presentation/pages/search_page.dart';
-import 'package:music_app/presentation/widgets/album_widget.dart';
 import 'package:music_app/presentation/widgets/appbar_search_button.dart';
 
  OpenLastAlbumSearchResultPage(WidgetTester tester,String songName)
@@ -21,12 +19,14 @@ async {
   tapFirstFavouriteButton(WidgetTester tester)
   async {
   final Finder favouriteButton = find.byType(FavoriteButton).first;
+
   await tester.tap(favouriteButton);
+  await tester.pumpAndSettle();
   final Finder albumWidget = find.byType(CachedNetworkImage).first;
   await tester.tap(albumWidget);
-  await tester.pageBack();
+  await tester.pumpAndSettle();
   await Future.delayed(const Duration(seconds:5));
-  await tester.tap(favouriteButton);
+  await tester.tap(find.byType(AppBar));
 }
 
 searchForAlbum(WidgetTester tester,String searchFieldText) async{
@@ -55,17 +55,5 @@ void main(){
     await tapFirstFavouriteButton(tester);
 
   });
-
-  // testWidgets("Click search button on homepage", (WidgetTester tester) async {
-  //   app.main();
-  //   await tester.pumpAndSettle();
-  //   final Finder titleApp = find.byType(Title);
-  //   final Finder buttonAppbarSearch = find.byType(Text);
-  //
-  //   await Future.delayed(const Duration(seconds:1));
-  //
-  //   await tester.tap(buttonAppbarSearch);
-  // });
-
 
 }
